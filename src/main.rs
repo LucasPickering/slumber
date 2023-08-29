9e5e0a6a91f39bc91f16382332395c3d5bbaf12d
Lucas Pickering <lucas@lucaspickering.me>
2023-08-29 08:15:30 -0400

    Add request list to UI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use crate::config::{
-    Environment, RequestCollection, RequestNode, RequestRecipe,
+use crate::{
+    config::{Environment, RequestCollection, RequestNode, RequestRecipe},
+    ui::App,
 };
 use anyhow::Context;
 use reqwest::{Client, Request, Response};
@@ -7,30 +8,32 @@ use std::collections::HashMap;
 
 mod config;
 mod template;
+mod ui;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let collection = RequestCollection::load(None).await?;
     println!("{collection:#?}");
     let engine = Engine::new(collection);
+    App::start(&engine.environments, &engine.recipes)?;
     let request = engine.build_request()?;
-    engine.execute_request(request).await?;
+    // engine.execute_request(request).await?;
     Ok(())
 }
 
 #[derive(Debug)]
 struct Engine {
-    recipes: RequestRecipes,
     environments: Vec<Environment>,
+    recipes: Vec<RequestRecipe>,
     http_client: Client,
     user_state: UserState,
 }
 
 #[derive(Debug)]
 struct UserState {
-    // TODO use string here instead
+    // TODO use strings here instead?
     selected_environment: Option<usize>,
-    selected_recipe: Option<String>,
+    selected_recipe: Option<usize>,
 }
 
 impl Engine {
@@ -38,11 +41,11 @@ impl Engine {
         let http_client = Client::new();
         let user_state = UserState {
             selected_environment: Some(0),
-            selected_recipe: Some("get-users".into()),
+            selected_recipe: Some(0),
         };
         Self {
             environments: collection.environments,
-            recipes: RequestRecipes::flatten(collection.requests).unwrap(),
+            recipes: flatten(collection.requests),
             http_client,
             user_state,
         }
@@ -57,8 +60,7 @@ impl Engine {
     fn recipe(&self) -> Option<&RequestRecipe> {
         self.user_state
             .selected_recipe
-            .as_ref()
-            .map(|id| &self.recipes.recipes[id])
+            .map(|index| &self.recipes[index])
     }
 
     fn build_request(&self) -> anyhow::Result<Request> {
@@ -81,40 +83,21 @@ impl Engine {
     }
 }
 
-#[derive(Debug)]
-struct RequestRecipes {
-    recipes: HashMap<String, RequestRecipe>,
-}
-
-impl RequestRecipes {
-    fn flatten(recipes: Vec<RequestNode>) -> anyhow::Result<Self> {
-        fn helper(
-            ass: &mut RequestRecipes,
-            recipes: Vec<RequestNode>,
-        ) -> anyhow::Result<()> {
-            for recipe in recipes {
-                match recipe {
-                    RequestNode::Folder { requests, .. } => {
-                        helper(ass, requests)?
-                    }
-                    RequestNode::Request(recipe) => {
-                        if ass
-                            .recipes
-                            .insert(recipe.id.clone(), recipe)
-                            .is_some()
-                        {
-                            todo!()
-                        }
-                    }
+fn flatten(recipes: Vec<RequestNode>) -> Vec<RequestRecipe> {
+    fn helper(all_recipes: &mut Vec<RequestRecipe>, recipes: Vec<RequestNode>) {
+        for recipe in recipes {
+            match recipe {
+                RequestNode::Folder { requests, .. } => {
+                    helper(all_recipes, requests)
+                }
+                RequestNode::Request(recipe) => {
+                    all_recipes.push(recipe);
                 }
             }
-            Ok(())
         }
-
-        let mut ass = Self {
-            recipes: HashMap::new(),
-        };
-        helper(&mut ass, recipes)?;
-        Ok(ass)
     }
+
+    let mut all_recipes = Vec::new();
+    helper(&mut all_recipes, recipes);
+    all_recipes
 }
---
+++ src/ui.rs
@@ -0,0 +1,207 @@
+use std::{
+    fmt::Display,
+    io::{self, Stdout},
+    time::{Duration, Instant},
+};
+
+use crate::config::{Environment, RequestRecipe};
+use crossterm::{
+    event::{
+        self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode,
+        KeyEventKind,
+    },
+    execute,
+    terminal::{
+        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
+        LeaveAlternateScreen,
+    },
+};
+use log::error;
+use ratatui::{prelude::*, widgets::*};
+
+struct StatefulList<T> {
+    state: ListState,
+    items: Vec<T>,
+}
+
+impl<T> StatefulList<T> {
+    fn with_items(items: Vec<T>) -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn unselect(&mut self) {
+        self.state.select(None);
+    }
+}
+
+/// This struct holds the current state of the app. In particular, it has the
+/// `items` field which is a wrapper around `ListState`. Keeping track of the
+/// items state let us render the associated widget with its state and have
+/// access to features such as natural scrolling.
+///
+/// Check the event handling at the bottom to see how to change the state on
+/// incoming events. Check the drawing logic for items on how to specify the
+/// highlighting style for selected items.
+pub struct App<'a> {
+    terminal: Terminal<CrosstermBackend<Stdout>>,
+    state: AppState<'a>,
+}
+
+struct AppState<'a> {
+    environments: StatefulList<&'a Environment>,
+    recipes: StatefulList<&'a RequestRecipe>,
+}
+
+impl<'a> App<'a> {
+    pub fn start(
+        environments: &'a [Environment],
+        recipes: &'a [RequestRecipe],
+    ) -> anyhow::Result<()> {
+        // Set up terminal
+        enable_raw_mode()?;
+        let mut stdout = io::stdout();
+        execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+        let backend = CrosstermBackend::new(stdout);
+        let terminal = Terminal::new(backend)?;
+
+        let app = App {
+            terminal,
+            state: AppState {
+                environments: StatefulList::with_items(
+                    environments.iter().collect(),
+                ),
+                recipes: StatefulList::with_items(recipes.iter().collect()),
+            },
+        };
+
+        app.run()
+    }
+
+    /// Run the main TUI update loop
+    fn run(mut self) -> anyhow::Result<()> {
+        let tick_rate = Duration::from_millis(250);
+        let mut last_tick = Instant::now();
+        loop {
+            self.terminal.draw(|f| ui(f, &mut self.state))?;
+
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+            if crossterm::event::poll(timeout)? {
+                if let Event::Key(key) = event::read()? {
+                    if key.kind == KeyEventKind::Press {
+                        match key.code {
+                            KeyCode::Char('q') => return Ok(()),
+                            KeyCode::Left => self.state.recipes.unselect(),
+                            KeyCode::Down => self.state.recipes.next(),
+                            KeyCode::Up => self.state.recipes.previous(),
+                            _ => {}
+                        }
+                    }
+                }
+            }
+
+            if last_tick.elapsed() >= tick_rate {
+                last_tick = Instant::now();
+            }
+        }
+    }
+}
+
+impl<'a> Drop for App<'a> {
+    fn drop(&mut self) {
+        // Restore terminal
+        log_error(disable_raw_mode());
+        log_error(execute!(
+            self.terminal.backend_mut(),
+            LeaveAlternateScreen,
+            DisableMouseCapture
+        ));
+        log_error(self.terminal.show_cursor());
+    }
+}
+
+/// If a result is an error, log it. Useful for handling errors in situations
+/// where we can't panic or exit.
+fn log_error<T, E: Display>(result: Result<T, E>) {
+    if let Err(err) = result {
+        error!("{err}");
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, state: &mut AppState) {
+    // Create layout
+    let chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Max(40), Constraint::Percentage(50)].as_ref())
+        .split(f.size());
+
+    // Create request list
+    let requests: Vec<ListItem> = state
+        .recipes
+        .items
+        .iter()
+        .map(|recipe| {
+            let lines = recipe.to_lines();
+            ListItem::new(lines)
+                .style(Style::default().fg(Color::Black).bg(Color::White))
+        })
+        .collect();
+
+    // Render request list
+    let items = List::new(requests)
+        .block(Block::default().borders(Borders::ALL).title("Requests"))
+        .highlight_style(
+            Style::default()
+                .bg(Color::LightGreen)
+                .add_modifier(Modifier::BOLD),
+        )
+        .highlight_symbol(">> ");
+
+    // We can now render the item list
+    f.render_stateful_widget(items, chunks[0], &mut state.recipes.state);
+}
+
+trait ToLines {
+    fn to_lines(&self) -> Vec<Line>;
+}
+
+impl ToLines for RequestRecipe {
+    fn to_lines(&self) -> Vec<Line> {
+        vec![
+            self.name.clone().into(),
+            format!("{} {}", self.method, self.url).into(),
+        ]
+    }
+}

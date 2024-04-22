81834dfaf526ddcbe10cdcf01ecaced9f471faaf
Lucas Pickering <lucas@lucaspickering.me>
2024-04-21 22:47:58 -0400

    Make folders collapsible in UI
    Closes #155

--- src/tui/view/component/recipe_list.rs
+++ src/tui/view/component/recipe_list.rs
@@ -1,14 +1,12 @@
-//! Recipe list
-
 use crate::{
-    collection::{Recipe, RecipeId, RecipeNode, RecipeTree},
+    collection::{Recipe, RecipeId, RecipeLookupKey, RecipeNode, RecipeTree},
     tui::{
         context::TuiContext,
         input::Action,
         view::{
-            common::{list::List, Pane},
+            common::Pane,
             draw::{Draw, Generate},
-            event::{Event, EventHandler, EventQueue},
+            event::{Event, EventHandler, EventQueue, Update},
             state::{
                 persistence::{Persistable, Persistent, PersistentKey},
                 select::{Dynamic, SelectState},
@@ -17,127 +15,270 @@ use crate::{
         },
     },
 };
+use derive_more::{Deref, DerefMut};
 use itertools::Itertools;
-use ratatui::{prelude::Rect, text::Span, Frame};
+use ratatui::{prelude::Rect, Frame};
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 
+/// List/tree of recipes and folders. This is mostly just a list, but with some
+/// extra logic to allow expanding/collapsing nodes. This could be made into a
+/// more generic component, but that adds abstraction that's not necessary
+/// because this is the only tree in the app. For similar reasons, we don't use
+/// the library tui-tree-widget, because it requires more abstraction that it
+/// saves us in code.
+///
+/// This implementation leans heavily on the fact that all nodes in the tree
+/// have a unique ID, which is another reason why it deserves its own
+/// implementation.
 #[derive(Debug)]
 pub struct RecipeListPane {
-    recipes: Component<Persistent<SelectState<Dynamic, RecipeListItem>>>,
+    /// A clone of the recipe tree
+    recipes: RecipeTree,
+    /// The visible list of items is tracked using normal list state, so we can
+    /// easily re-use existing logic. We'll rebuild this any time a folder is
+    /// expanded/collapsed (i.e whenever the list of items changes)
+    select_state: Component<Persistent<SelectState<Dynamic, RecipeNode>>>,
+    /// Set of all folders that are collapsed
+    /// Invariant: No recipes, only folders
+    collapsed: Persistent<Collapsed>,
 }
 
 pub struct RecipeListPaneProps {
     pub is_selected: bool,
 }
 
-/// Each folder/recipe in the list, plus metadata
-#[derive(Debug)]
-struct RecipeListItem {
-    node: RecipeNode,
-    depth: usize,
+/// Set of collapsed folders. This newtype is really only necessary so we can
+/// implement [Persistable] on it
+#[derive(Debug, Default, Deref, DerefMut, Serialize, Deserialize)]
+#[serde(transparent)]
+struct Collapsed(HashSet<RecipeId>);
+
+/// Ternary state for modifying node collapse state
+enum CollapseState {
+    Expand,
+    Collapse,
+    Toggle,
 }
 
 impl RecipeListPane {
     pub fn new(recipes: &RecipeTree) -> Self {
-        // When highlighting a new recipe, load it from the repo
-        fn on_select(_: &mut RecipeListItem) {
-            // If a recipe isn't selected, this will do nothing
-            EventQueue::push(Event::HttpLoadRequest);
-        }
-
-        // Flatten the tree into a list
-        let recipes = recipes
-            .iter()
-            .map(|(lookup_key, node)| RecipeListItem {
-                node: node.clone(),
-                depth: lookup_key.len() - 1,
-            })
-            .collect_vec();
-
+        // This clone is unfortunate, but we can't hold onto a reference to the
+        // recipes
+        let collapsed = Persistent::new(
+            PersistentKey::RecipeCollapsed,
+            Collapsed::default(),
+        );
+        let persistent = Persistent::new(
+            PersistentKey::RecipeId,
+            build_select_state(recipes, &collapsed),
+        );
         Self {
-            recipes: Persistent::new(
-                PersistentKey::RecipeId,
-                SelectState::new(recipes).on_select(on_select),
-            )
-            .into(),
+            recipes: recipes.clone(),
+            select_state: persistent.into(),
+            collapsed,
         }
     }
 
     /// Which recipe in the recipe list is selected? `None` iff the list is
     /// empty OR a folder is selected.
     pub fn selected_recipe(&self) -> Option<&Recipe> {
-        self.recipes
-            .selected()
-            .and_then(|list_item| list_item.node.recipe())
+        self.select_state.selected().and_then(RecipeNode::recipe)
+    }
+
+    /// Set the currently selected folder as expanded/collapsed (or toggle it).
+    /// If a folder is not selected, do nothing. Returns whether a change was
+    /// made.
+    fn set_selected_collapsed(&mut self, state: CollapseState) -> bool {
+        let folder = self.select_state.selected().and_then(RecipeNode::folder);
+        let changed = if let Some(folder) = folder {
+            let collapsed = &mut self.collapsed;
+            match state {
+                CollapseState::Expand => collapsed.remove(&folder.id),
+                CollapseState::Collapse => collapsed.insert(folder.id.clone()),
+                CollapseState::Toggle => {
+                    if collapsed.contains(&folder.id) {
+                        collapsed.remove(&folder.id);
+                    } else {
+                        collapsed.insert(folder.id.clone());
+                    }
+                    true
+                }
+            }
+        } else {
+            false
+        };
+
+        // If we changed the set of what is visible, rebuild the list state
+        if changed {
+            let mut new_select_state =
+                build_select_state(&self.recipes, &self.collapsed);
+            // Carry over the selection
+            if let Some(selected) = self.select_state.selected() {
+                new_select_state.select(selected.id());
+            }
+            **self.select_state = new_select_state;
+        }
+
+        changed
     }
 }
 
 impl EventHandler for RecipeListPane {
+    fn update(&mut self, event: Event) -> Update {
+        if let Event::Input {
+            action: Some(action),
+            ..
+        } = event
+        {
+            match action {
+                Action::Left => {
+                    self.set_selected_collapsed(CollapseState::Collapse);
+                }
+                Action::Right => {
+                    self.set_selected_collapsed(CollapseState::Expand);
+                }
+                Action::Submit => {
+                    if !self.set_selected_collapsed(CollapseState::Toggle) {
+                        // Propgate submit event for recipes, so it launches a
+                        // request
+                        return Update::Propagate(event);
+                    }
+                }
+                _ => return Update::Propagate(event),
+            };
+        } else {
+            return Update::Propagate(event);
+        }
+        Update::Consumed
+    }
+
     fn children(&mut self) -> Vec<Component<&mut dyn EventHandler>> {
-        vec![self.recipes.as_child()]
+        vec![self.select_state.as_child()]
     }
 }
 
 impl Draw<RecipeListPaneProps> for RecipeListPane {
     fn draw(&self, frame: &mut Frame, props: RecipeListPaneProps, area: Rect) {
-        self.recipes.set_area(area); // Needed for tracking cursor events
-        let title = TuiContext::get()
+        let context = TuiContext::get();
+        self.select_state.set_area(area); // Needed for tracking cursor events
+
+        let title = context
             .input_engine
             .add_hint("Recipes", Action::SelectRecipeList);
-        let list = List {
-            block: Some(Pane {
-                title: &title,
-                is_focused: props.is_selected,
-            }),
-            list: &self.recipes,
+        let pane = Pane {
+            title: &title,
+            is_focused: props.is_selected,
         };
+
+        // We have to build this manually instead of using our own List type,
+        // because we need outside context during the render
+        let items = self
+            .select_state
+            .items()
+            .iter()
+            .map(|node| {
+                let (icon, name) = match node {
+                    RecipeNode::Folder(folder) => {
+                        let icon = if self.collapsed.is_collapsed(&folder.id) {
+                            "▶"
+                        } else {
+                            "▼"
+                        };
+                        (icon, folder.name())
+                    }
+                    RecipeNode::Recipe(recipe) => ("", recipe.name()),
+                };
+                let depth = self
+                    .recipes
+                    .get_lookup_key(node.id())
+                    .unwrap_or_else(|| {
+                        panic!("Recipe node {} is not in tree", node.id())
+                    })
+                    .len()
+                    - 1;
+
+                // Apply indentation
+                format!(
+                    "{indent:width$}{icon}{name}",
+                    indent = "",
+                    width = depth
+                )
+            })
+            .collect_vec();
+        let list = ratatui::widgets::List::new(items)
+            .block(pane.generate())
+            .highlight_style(context.theme.list.highlight);
+
         frame.render_stateful_widget(
-            list.generate(),
+            list,
             area,
-            &mut self.recipes.state_mut(),
-        )
+            &mut self.select_state.state_mut(),
+        );
+    }
+}
+
+impl Collapsed {
+    /// Is this specific folder collapsed?
+    fn is_collapsed(&self, folder_id: &RecipeId) -> bool {
+        self.0.contains(folder_id)
+    }
+
+    /// Is the given node visible? This takes lookup key so it can check all
+    /// ancestors for visibility too.
+    fn is_visible(&self, lookup_key: &RecipeLookupKey) -> bool {
+        // If any ancestors are collapsed, this is *not* visible
+        let [ancestors @ .., _] = lookup_key.as_slice() else {
+            panic!("Recipe lookup key cannot be empty")
+        };
+        !ancestors.iter().any(|id| self.is_collapsed(id))
     }
 }
 
 /// Persist recipe by ID
-impl Persistable for RecipeListItem {
+impl Persistable for RecipeNode {
     type Persisted = RecipeId;
 
     fn get_persistent(&self) -> &Self::Persisted {
-        self.node.id()
+        self.id()
     }
 }
 
 /// Needed for persistence loading
-impl PartialEq<RecipeListItem> for RecipeId {
-    fn eq(&self, other: &RecipeListItem) -> bool {
-        self == other.node.id()
+impl PartialEq<RecipeNode> for RecipeId {
+    fn eq(&self, node: &RecipeNode) -> bool {
+        self == node.id()
     }
 }
 
-impl Generate for &RecipeListItem {
-    type Output<'this> = Span<'this> where Self: 'this;
+/// Persistence for collapsed set of folders. Technically this can accrue
+/// removed folders over time (if they were collapsed at the time of deletion).
+/// That isn't really an issue though, it just means it'll be pre-collapsed if
+/// the user ever adds the folder back. Not worth working around.
+impl Persistable for Collapsed {
+    type Persisted = Self;
 
-    fn generate<'this>(self) -> Self::Output<'this>
-    where
-        Self: 'this,
-    {
-        // Apply indentation based on folder depth
-        let content = format!(
-            "{indent:width$}{name}",
-            indent = "",
-            name = self.node.name(),
-            width = self.depth
-        );
-
-        let theme = &TuiContext::get().theme;
-        let style = match self.node {
-            RecipeNode::Folder(_) => theme.recipe_list.folder,
-            RecipeNode::Recipe(_) => theme.recipe_list.recipe,
-        };
-
-        Span {
-            content: content.into(),
-            style,
-        }
+    fn get_persistent(&self) -> &Self::Persisted {
+        self
     }
+}
+
+/// Construct select list based on which nodes are currently visible
+fn build_select_state(
+    recipes: &RecipeTree,
+    collapsed: &Collapsed,
+) -> SelectState<Dynamic, RecipeNode> {
+    // When highlighting a new recipe, load it from the repo
+    fn on_select(_: &mut RecipeNode) {
+        // If a recipe isn't selected, this will do nothing
+        EventQueue::push(Event::HttpLoadRequest);
+    }
+
+    let items = recipes
+        .iter()
+        // Filter out hidden nodes
+        .filter(|(lookup_key, _)| collapsed.is_visible(lookup_key))
+        .map(|(_, node)| node.clone())
+        .collect();
+    SelectState::new(items).on_select(on_select)
 }

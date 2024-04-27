a3fefd1802691c515bb66a919ecf718925971c35
Ben Faerber <benfaerber@protonmail.com>
2024-04-27 16:47:21 -0600

    Add nanohttp

--- src/collection/jetbrains.rs
+++ src/collection/jetbrains.rs
@@ -2,19 +2,31 @@
 
 use std::collections::HashMap;
 
+use nanohttp::{Header, Method, Path, Request};
 use nom::{
-    branch::alt, bytes::complete::{tag, take_till, take_until}, character::{complete::{alpha1,alphanumeric1, anychar, char, multispace0, multispace1, newline, one_of, space0, space1}, is_space}, combinator::{not, opt, recognize}, error::{Error as NomError, ErrorKind, ParseError, VerboseError}, multi::{many0_count, many1}, sequence::{delimited, pair, tuple}, FindSubstring, Finish, IResult, InputLength, InputTake, Offset, Parser
+    branch::alt,
+    bytes::complete::{tag, take_till, take_until},
+    character::{
+        complete::{
+            alpha1, alphanumeric1, anychar, char, multispace0, multispace1,
+            newline, one_of, space0, space1,
+        },
+        is_space,
+    },
+    combinator::{not, opt, recognize},
+    error::{Error as NomError, ErrorKind, ParseError, VerboseError},
+    multi::{many0_count, many1},
+    sequence::{delimited, pair, tuple},
+    Parser, IResult,
 };
-use httparse::{self};
 
 /// Notes:
 /// for line in lines:
 ///     parse_separator: check for ###, check if name exists `### RequestName`
 ///     parse name annotation: m match `# @name=Name` or `# @name Name`
-///     parse_comment: `hello hello // comment`, `hello # comment`, `// comment`, '# comment'
+///     parse_comment: `hello # comment`, `// comment`, '# comment'
 ///     parse_variable: @x = y
-///     parse_request: build up the request line by line, fill in the variables 
-
+///     parse_request: build up the request line by line, fill in the variables
 
 type StrResult<'a> = Result<(&'a str, &'a str), nom::Err<NomError<&'a str>>>;
 
@@ -24,9 +36,14 @@ const NAME_ANNOTATION: &str = "@name";
 const VARIABLE_OPEN: &str = "{{";
 const VARIABLE_CLOSE: &str = "}}";
 
+/// The variable literals are replaced with URL safe characters so `nanohttp` can parse it as
+/// normal
+const PLACEHOLDER_VARIABLE_OPEN: &str = "VAROPEN";
+const PLACEHOLDER_VARIABLE_CLOSE: &str = "VARCLOSE";
+
 #[derive(Debug, Clone, PartialEq)]
 enum Line {
-    Seperator,
+    Seperator(Option<String>),
     Name(String),
     Request(String),
 }
@@ -34,39 +51,31 @@ enum Line {
 #[derive(Debug, Clone, PartialEq)]
 struct JetbrainsRequest {
     name: Option<String>,
-    request: String,
+    method: Method,
+    path: Path,
+    version: String,
+    scheme: String,
+    headers: Vec<Header>,
 }
 
 impl JetbrainsRequest {
-    fn is_empty(&self) -> bool {
-        self.request == "" 
-    }
-
-    fn push_line(&mut self, value: &str) -> &mut Self {
-        self.request.push_str(&format!("{value}\r\n"));
-        self
-    }
-
-    fn set_name(&mut self, name: &str) -> &mut Self {
-        self.name = Some(name.into());
-        self
-    }
-
-    fn trim(&mut self) -> &mut Self {
-        self.request.trim();
-        self
-    }
-}
-
-impl Default for JetbrainsRequest {
-    fn default() -> Self {
+    fn from(name: Option<String>, raw_request: &str) -> Self {
+        let request = Request::from_string(raw_request).unwrap();
+        let headers = request.headers;
+        let method = request.method;
+        let path = request.path;
+        let scheme = request.scheme;
+        let version = request.version;
         Self {
-            name: None,
-            request: "".into(),
+            name,
+            method,
+            path,
+            version,
+            scheme,
+            headers,
         }
     }
 }
-
 
 #[derive(Debug, Clone, PartialEq)]
 struct JetbrainsHttp {
@@ -74,40 +83,36 @@ struct JetbrainsHttp {
     variables: HashMap<String, String>,
 }
 
-fn parse_seperator(input: &str) -> IResult<&str, Vec<Line>> {
+fn parse_seperator(input: &str) -> IResult<&str, Option<String>> {
     let (input, _) = tag(REQUEST_DELIMITER)(input)?;
-    let (input, req_name) = opt(pair(
-        space1,
-        take_till(|c| c == ' ' || c == '\n')
-    ))(input)?;
+    let (input, req_name) =
+        opt(pair(space1, take_till(|c| c == ' ' || c == '\n')))(input)?;
 
-    let got = match req_name {
-        Some((_, name)) => vec![Line::Seperator, Line::Name(name.into())],
-        None => vec![Line::Seperator],
-    };
-    Ok((input, got))
+    let potential_name = req_name.map(|(_, name)| name.to_string());
+    Ok((input, potential_name))
 }
 
-fn parse_request_name_annotation(input: &str) -> IResult<&str, Line> {
+fn parse_request_name_annotation(input: &str) -> IResult<&str, &str> {
     let (input, _) = pair(char('#'), space0)(input)?;
     let (input, _) = tag(NAME_ANNOTATION)(input)?;
     let (input, _) = pair(alt((char('='), char(' '))), space0)(input)?;
-    let (input, req_name) = take_till(|c| c == ' ' || c == '\n')(input)?; 
+    let (input, req_name) = take_till(|c| c == ' ' || c == '\n')(input)?;
 
-    Ok((input, Line::Name(req_name.into())))
+    Ok((input, req_name.into()))
 }
 
 fn parse_variable_identifier(input: &str) -> IResult<&str, &str> {
     recognize(pair(
         alpha1,
-        many0_count(alt((alphanumeric1, tag("_"), tag("-"), tag("."))))
-    )).parse(input)
+        many0_count(alt((alphanumeric1, tag("_"), tag("-"), tag(".")))),
+    ))
+    .parse(input)
 }
 
 /// Parses an HTTP File variable (@MY_VAR = 1234)
 fn parse_variable_assignment(input: &str) -> IResult<&str, (&str, &str)> {
     let (input, _) = char('@')(input)?;
-    let (input, id) = parse_variable_identifier(input)?; 
+    let (input, id) = parse_variable_identifier(input)?;
 
     let (input, _) = tuple((opt(space1), char('='), opt(space1)))(input)?;
     let (input, value) = take_till(|c| c == '\n')(input)?;
@@ -123,8 +128,8 @@ fn starting_slash_comment(line: &str) -> StrResult {
 fn parse_line_without_comment(line: &str) -> StrResult {
     // A comment can start with `//` but it cant be in the middle
     // This would prevent you from writing urls: `https://`
-    if let Ok((inp, _)) = starting_slash_comment(line) { 
-        return Ok((inp, ""))
+    if let Ok((inp, _)) = starting_slash_comment(line) {
+        return Ok((inp, ""));
     }
 
     take_until("#")(line)
@@ -142,42 +147,18 @@ fn until_variable_open(input: &str) -> StrResult {
     take_until(VARIABLE_OPEN)(input)
 }
 
-fn parse_request_line<'a>(line: &'a str, variables: &'a HashMap<String, String>) -> IResult<&'a str, Line> {
-    let mut request_line: String = "".into();
-    let mut input = line;
-    loop {
-        if let Ok((rest, var)) = parse_variable_substitution(input) {
-            let value = variables.get(var).unwrap();
-            input = rest;
-            request_line += value;
-            continue
-        }
-
-        if let Ok((rest, got)) = until_variable_open(input) { 
-            input = rest;
-            request_line += got;
-            continue;
-        }
-        
-        break;
-    }
-
-    Ok((input, Line::Request(request_line.into()))) 
-} 
-
-
 fn parse_lines(input: &str) -> (Vec<Line>, HashMap<String, String>) {
     let mut lines: Vec<Line> = vec![];
     let mut variables: HashMap<String, String> = HashMap::new();
     for line in input.trim().lines() {
         let line = &format!("{line}\n");
-        if let Ok((_, sep_lines)) = parse_seperator(line) {
-            lines.extend(sep_lines);
+        if let Ok((_, seperator_name)) = parse_seperator(line) {
+            lines.push(Line::Seperator(seperator_name));
             continue;
         }
 
         if let Ok((_, name)) = parse_request_name_annotation(line) {
-            lines.push(name);
+            lines.push(Line::Name(name.into()));
             continue;
         }
 
@@ -191,39 +172,59 @@ fn parse_lines(input: &str) -> (Vec<Line>, HashMap<String, String>) {
         }
 
         if line != "\n" {
-            lines.push(Line::Request(line.into()));
+            let placeholder_line = line;
+                // .replace(VARIABLE_OPEN, PLACEHOLDER_VARIABLE_OPEN)
+                // .replace(VARIABLE_CLOSE, PLACEHOLDER_VARIABLE_CLOSE);
+            lines.push(Line::Request(placeholder_line.into()));
         }
     }
     (lines, variables)
 }
 
-fn build_file_from_lines(lines: Vec<Line>, variables: HashMap<String, String>) -> JetbrainsHttp {
-    let mut sections: Vec<JetbrainsRequest> = vec![];
-    let mut current_request: JetbrainsRequest = JetbrainsRequest::default();
-    for line in lines {
-        match line {
-            Line::Seperator => {
-                if !current_request.is_empty() {
-                    current_request.trim();
-                    sections.push(current_request);
+impl JetbrainsHttp {
+    fn from_lines(
+        lines: Vec<Line>,
+        variables: HashMap<String, String>,
+    ) -> Self {
+        let mut sections: Vec<JetbrainsRequest> = vec![];
+        let mut current_name: Option<String> = None;
+        let mut current_request: String = "".into();
+        for line in lines {
+            match line {
+                Line::Seperator(name_opt) => {
+                    if current_request != "" {
+                        let request = JetbrainsRequest::from(
+                            current_name,
+                            &current_request,
+                        );
+                        sections.push(request);
+                    }
+
+                    current_name = None;
+                    current_request = "".into();
+
+                    if let Some(name) = name_opt {
+                        current_name = Some(name);
+                    }
                 }
-                current_request = JetbrainsRequest::default();
-            },
-            Line::Name(name) => {
-                current_request.set_name(&name);
-            },
-            Line::Request(req) => {
-                current_request.push_line(&req);
+                Line::Name(name) => {
+                    current_name = Some(name);
+                }
+                Line::Request(req) => {
+                    current_request.push_str(&format!("{req}\r\n"));
+                }
             }
         }
-    }
 
-    JetbrainsHttp {
-        sections,
-        variables,
+        let request = JetbrainsRequest::from(current_name, &current_request);
+        sections.push(request);
+
+        Self {
+            sections,
+            variables,
+        }
     }
 }
-
 
 #[cfg(test)]
 mod test {
@@ -234,10 +235,7 @@ mod test {
         let example_var = "@MY_VAR    = 1231\n";
         let (_, var) = parse_variable_assignment(example_var).unwrap();
 
-        assert_eq!(
-            var,
-            ("MY_VAR", "1231"),
-        );
+        assert_eq!(var, ("MY_VAR", "1231"),);
 
         let example_var = "@MY_NAME =hello\n";
         let (rest, var) = parse_variable_assignment(example_var).unwrap();
@@ -256,16 +254,16 @@ mod test {
     #[test]
     fn parse_seperator_line() {
         let line = "### RequestName";
-        let (_, items) = parse_seperator(line).unwrap();
-        assert_eq!(items, vec![Line::Seperator, Line::Name("RequestName".into())]);
+        let (_, name_opt) = parse_seperator(line).unwrap();
+        assert_eq!(name_opt, Some("RequestName".into()));
 
         let line = "#######";
-        let (_, items) = parse_seperator(line).unwrap();
-        assert_eq!(items, vec![Line::Seperator]);
+        let (_, name_opt) = parse_seperator(line).unwrap();
+        assert_eq!(name_opt, None);
 
         let line = "###";
-        let (_, items) = parse_seperator(line).unwrap();
-        assert_eq!(items, vec![Line::Seperator]);
+        let (_, name_opt) = parse_seperator(line).unwrap();
+        assert_eq!(name_opt, None);
 
         let line = "#";
         let res = parse_seperator(line);
@@ -276,13 +274,12 @@ mod test {
     fn parse_request_name_test() {
         let line = "# @name=hello";
         let (_, name) = parse_request_name_annotation(line).unwrap();
-        assert_eq!(name, Line::Name("hello".into()));  
+        assert_eq!(name, "hello".to_string());
 
         let line = "# @name Cool";
         let (_, name) = parse_request_name_annotation(line).unwrap();
-        assert_eq!(name, Line::Name("Cool".into()));  
+        assert_eq!(name, "Cool".to_string());
     }
-
 
     #[test]
     fn parse_lines_test() {
@@ -290,20 +287,22 @@ mod test {
 ###
 @MY_VAR = 123
 @hello=blahblah
-GET https://httpbin.org HTTP/1.1
+GET https://httpbin1.org HTTP/1.1
+Host: localhost
 
 // Comment
 @var = 12
 
-### Request
+### HelloHttpBinRequest
 
 GET {{hello}} HTTP/1.1
 
 example example
 ######
-# @name OtherRequest
+# @name JSONRequest
 
-POST {{HOST}}/post HTTP/1.1
+POST /post HTTP/1.1
+Host: localhost
 Content-Type: application/json
 X-Http-Method-Override: PUT
 
@@ -311,11 +310,15 @@ X-Http-Method-Override: PUT
     "data": "my data"
 }
         "#;
-    
+
         let (lines, variables) = parse_lines(example);
         println!("{:?}", lines);
 
-        let file = build_file_from_lines(lines, variables);
-        println!("{:?}", file);
+        let file = JetbrainsHttp::from_lines(lines, variables);
+        let output = format!("{:?}", file.sections);
+        println!(
+            "{}",
+            output.replace("JetbrainsRequest {", "\nJetbrainsRequest {")
+        );
     }
 }

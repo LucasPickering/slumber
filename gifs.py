9e89cef0d75c67e911bc293fd05321d1d92061be
Lucas Pickering <lucas@lucaspickering.me>
2025-01-01 11:31:21 -0500

    Add support for multiple tape->gif generation

---
+++ gifs.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+"""
+Generate GIFs from VHS tapes
+"""
+
+import argparse
+import glob
+import os
+import subprocess
+
+TAPE_DIR = "tapes/"
+GIF_DIR = "static/"
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Generate GIFs for docs")
+    parser.add_argument(
+        "--check", action="store_true", help="Check if all GIFs are up to date"
+    )
+    args = parser.parse_args()
+
+    if args.check:
+        check_all()
+    else:
+        generate_all()
+
+
+def generate_all() -> None:
+    tapes = get_tapes()
+    print(f"Generating GIFs for: {tapes}")
+    run(["cargo", "build"])
+    for tape in tapes:
+        generate(tape)
+    print("Don't forget to check all GIFs before pushing!")
+
+
+def generate(tape: str) -> None:
+    gif = get_gif_path(tape)
+    print(f"Generating {tape} -> {gif}")
+    run(["vhs", tape])
+
+
+def check_all() -> None:
+    latest_commit = run(["git", "rev-parse", "HEAD"])
+    tapes = get_tapes()
+    failed = []
+    for tape in tapes:
+        gif = get_gif_path(tape)
+        good = check(gif_path=gif, latest_commit=latest_commit)
+        if not good:
+            failed.append(gif)
+        print(f"  {tape} -> {gif}: {'PASS' if good else 'FAIL'}")
+    if failed:
+        raise Exception(f"Some GIFs are outdated: {failed}")
+    else:
+        print("All GIFs are up to date")
+
+
+def check(gif_path: str, latest_commit: str) -> bool:
+    """Check if the GIF is outdated"""
+    latest_gif_commit = run(
+        ["git", "log", "-n", "1", "--pretty=format:%H", "--", gif_path]
+    )
+    return latest_commit == latest_gif_commit
+
+
+def get_tapes() -> list[str]:
+    return glob.glob(os.path.join(TAPE_DIR, "*"))
+
+
+def get_gif_path(tape_path: str) -> str:
+    file_name = os.path.basename(tape_path)
+    (name, _) = os.path.splitext(file_name)
+    return os.path.join(GIF_DIR, f"{name}.gif")
+
+
+def run(command: list[str]) -> str:
+    output = subprocess.check_output(command)
+    return output.decode().strip()
+
+
+if __name__ == "__main__":
+    main()

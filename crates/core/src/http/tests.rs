c78b0469f14063faa41c10c10aec83e2abbaee23
Lucas Pickering <lucas@lucaspickering.me>
2025-05-22 18:20:38 -0400

    Use enum for representing query param values
    Instead of support a list of param=value strings, we now support a map of {param: value} OR {param: [value, ...]}. This avoids string parsing, is easier to deal with internally,and is more consistent with the rest of the collection format.
    
    This is obviously a breaking change. It will be bundled with other breaking changes and an importer will convert from the old format to the new one.

--- crates/core/src/http/tests.rs
+++ crates/core/src/http/tests.rs
@@ -124,10 +124,10 @@ async fn test_build_request(http_engine: HttpEngine) {
     let recipe = Recipe {
         method: HttpMethod::Post,
         url: "{{host}}/users/{{user_id}}".into(),
-        query: vec![
-            ("mode".into(), "{{mode}}".into()),
-            ("fast".into(), "true".into()),
-        ],
+        query: indexmap! {
+            "mode".into() => "{{mode}}".into(),
+            "fast".into() => "true".into(),
+        },
         headers: indexmap! {
             // Leading/trailing newlines should be stripped
             "Accept".into() => "application/json".into(),
@@ -184,12 +184,10 @@ async fn test_build_request(http_engine: HttpEngine) {
 async fn test_build_url(http_engine: HttpEngine) {
     let recipe = Recipe {
         url: "{{host}}/users/{{user_id}}".into(),
-        query: vec![
-            ("mode".into(), "{{mode}}".into()),
-            ("fast".into(), "true".into()),
-            ("fast".into(), "false".into()),
-            ("mode".into(), "user".into()),
-        ],
+        query: indexmap! {
+            "mode".into() => ["{{mode}}", "user"].into(),
+            "fast".into() => ["true", "false"].into(),
+        },
         ..Recipe::factory(())
     };
     let context = template_context(recipe);
@@ -198,7 +196,7 @@ async fn test_build_url(http_engine: HttpEngine) {
 
     assert_eq!(
         url.as_str(),
-        "http://localhost/users/1?mode=sudo&fast=true&fast=false&mode=user"
+        "http://localhost/users/1?mode=sudo&mode=user&fast=true&fast=false"
     );
 }
 
@@ -487,14 +485,15 @@ async fn test_override_headers(http_engine: HttpEngine) {
 async fn test_override_query(http_engine: HttpEngine) {
     let recipe = Recipe {
         url: "http://localhost/url".into(),
-        query: vec![
+        query: indexmap! {
             // Overridden
-            ("mode".into(), "regular".into()),
+            "mode".into() => "regular".into(),
             // Excluded
-            ("fast".into(), "false".into()),
-            // Included
-            ("fast".into(), "true".into()),
-        ],
+            "fast".into() => [
+                "false", // Excluded
+                "true", // Included
+            ].into(),
+        },
         ..Recipe::factory(())
     };
     let context = template_context(recipe);
@@ -744,11 +743,10 @@ fn test_trim_bytes(#[case] bytes: &[u8], #[case] expected: &[u8]) {
 async fn test_build_curl(http_engine: HttpEngine) {
     let recipe = Recipe {
         method: HttpMethod::Get,
-        query: vec![
-            ("mode".into(), "{{mode}}".into()),
-            ("fast".into(), "true".into()),
-            ("fast".into(), "false".into()),
-        ],
+        query: indexmap! {
+            "mode".into() => "{{mode}}".into(),
+            "fast".into() => ["true", "false"].into(),
+        },
         headers: indexmap! {
             "Accept".into() => "application/json".into(),
             "Content-Type".into() => "application/json".into(),

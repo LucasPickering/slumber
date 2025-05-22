04ab09946ef8c76e04a164d812f4b7345f4486a9
Lucas Pickering <lucas@lucaspickering.me>
2025-05-22 16:27:10 -0400

    Ignore any top-level YAML field beginning with .
    This applies to both the collection and config formats. This is not a breaking change because the allowable set of fields is a superset of the previously allowed fields.

--- crates/import/src/rest.rs
+++ crates/import/src/rest.rs
@@ -5,7 +5,6 @@
 use anyhow::anyhow;
 use indexmap::IndexMap;
 use itertools::Itertools;
-use serde::de::IgnoredAny;
 use slumber_core::{
     collection::{
         Authentication, Chain, ChainId, ChainOutputTrim, ChainSource,
@@ -321,7 +320,6 @@ fn build_collection(rest_format: RestFormat) -> Collection {
         profiles,
         chains,
         recipes,
-        _ignore: IgnoredAny,
     }
 }
 

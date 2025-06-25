b9b19fb9f10c7c02c26c7bf4ba46f42f723f73ad
Lucas Pickering <lucas@lucaspickering.me>
2025-06-24 21:12:06 -0400

    Replace chains with functions in templates
    This is a major change to the template syntax. It should make it much more intuitive and composable. It makes it possible to write dynamic templates inline without having to define a chain elsewhere.
    
    This is still buggy and some tests are failing, but it's 90% of the way there.

--- crates/tui/src/state.rs
+++ crates/tui/src/state.rs
@@ -16,8 +16,9 @@ use slumber_core::{
     collection::{Collection, CollectionFile, ProfileId},
     database::CollectionDatabase,
     http::{Exchange, RequestError, RequestId, RequestSeed},
-    template::{Prompter, Template, TemplateChunk, TemplateContext},
+    render::{Prompter, TemplateContext},
 };
+use slumber_template::{RenderedChunk, Template};
 use slumber_util::ResultTraced;
 use std::{path::Path, sync::Arc, time::Duration};
 use tokio::task;
@@ -600,7 +601,7 @@ impl LoadedState {
         &self,
         template: Template,
         profile_id: Option<ProfileId>,
-        on_complete: Callback<Vec<TemplateChunk>>,
+        on_complete: Callback<Vec<RenderedChunk>>,
     ) {
         let context = self.template_context(profile_id, true);
         util::spawn(async move {
@@ -633,6 +634,7 @@ impl LoadedState {
             http_provider: Box::new(http_provider),
             prompter,
             overrides: Default::default(),
+            show_sensitive: !is_preview,
             state: Default::default(),
         }
     }
